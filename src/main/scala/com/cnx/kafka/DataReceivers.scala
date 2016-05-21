5685903e1ed4e94cab07746c54971f21176ffc3b
qiang <xieqiangjob@qq.com>
2016-05-22 05:06:18 +0800

    add plugins.sbt for assembly

--- src/main/scala/com/cnx/kafka/DataReceivers.scala
+++ src/main/scala/com/cnx/kafka/DataReceivers.scala
@@ -2,6 +2,7 @@ package com.cnx.kafka
 import kafka.serializer.StringDecoder
 import org.apache.spark._
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{StreamingContext, _}
 import org.apache.spark.streaming.kafka.{KafkaUtils, _}
 import org.apache.spark.streaming.scheduler.StreamingListener
@@ -28,10 +29,12 @@ class DataReceivers(sc: SparkContext) {
     return null;
   }
   
-  def directlyDataFromKafka(topic:String): DataFrame={
+  def directlyDataFromKafka(topic:String) ={
     
     //create spark streaming..
-    val ssc = new StreamingContext(sc, Seconds(2))
+    val ssc = new StreamingContext(sc, Seconds(5))
+    ssc.checkpoint("/tmp")
+
     // Define the Kafka parameters, broker list must be specified
     //val kafkaParams = Map[String, String]("metadata.broker.list" -> "192.168.1.111:9092",
     //  "group.id" -> "test-consumer-group",
@@ -46,22 +49,33 @@ class DataReceivers(sc: SparkContext) {
     // Create the direct stream with the Kafka parameters and topics
     val kafkaStream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
 
-    System.out.println(kafkaStream.toString())
+    kafkaStream.foreachRDD(rdd => {
+
+      rdd.foreach{
+
+        data=>  System.out.println("=============="+data.toString()) }
+
+    })
+
+
     //val kafkaStream = KafkaUtils.createDirectStream[Array[Byte], Array[Byte], DefaultDecoder, DefaultDecoder](ssc, kafkaParams, topics)
-  
+
     // Define the offset ranges to read in the batch job
-    val offsetRanges = Array(
-      OffsetRange(topic, 0, 10, 2000000))
+    /**val offsetRanges = Array(
+      OffsetRange(topic, 0, 100, 200))
       // Create the RDD based on the offset ranges
-     val rdd = KafkaUtils.createRDD[String, String, StringDecoder, StringDecoder](sc, kafkaParams, offsetRanges)
+    val rdd = KafkaUtils.createRDD[String, String, StringDecoder, StringDecoder](sc, kafkaParams, offsetRanges)
  
 
-     rdd.foreachPartition ( partitionOfRecords => partitionOfRecords.foreach(System.out.println))
+    rdd.foreachPartition ( partitionOfRecords => partitionOfRecords.foreach(System.out.println))
+    // rdd.foreachPartition ( partitionOfRecords => partitionOfRecords.foreach(System.out.println))
+    // rdd.foreachPartition ( partitionOfRecords => partitionOfRecords.foreach(System.out.println))
+
+      **/
+    ssc.start()
+    ssc.awaitTermination()
     
-    //ssc.start()
-    //ssc.awaitTermination()
-    
-     return null
+
   }
 }
 
