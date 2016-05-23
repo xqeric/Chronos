27f9db590ce6f86c7a2b73c237ee8b457d36a40c
qiang <xieqiangjob@qq.com>
2016-05-24 07:30:37 +0800

    add plugins.sbt for assembly

--- src/main/scala/com/cnx/kafka/DataReceivers.scala
+++ src/main/scala/com/cnx/kafka/DataReceivers.scala
@@ -6,6 +6,8 @@ import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{StreamingContext, _}
 import org.apache.spark.streaming.kafka.{KafkaUtils, _}
 import org.apache.spark.streaming.scheduler.StreamingListener
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.cql.CassandraConnector
 
 //import  org.apache.kafka.common.message. //kafka.serializer.StringDecoder
 
@@ -14,34 +16,22 @@ import org.apache.spark.streaming.scheduler.StreamingListener
  * 
  */
 class DataReceivers(sc: SparkContext) {
-  
-  /**
-   * access the kakfa data service
-   */
-  def receiveDataFromKafka(topic:String):DataFrame = {
-    //create spark streaming..
-    val ssc: StreamingContext = new StreamingContext(sc, Seconds(2))
-    val mytopic = topic.map { x => x }
-    //val kafkaParams = Map("metadata.broker.list" -> "172.17.42.1:9092", "metadata.broker.list" -> "172.17.42.1:9092")
-    //val kafkaStream = KafkaUtils.createStream(ssc, kafkaParams, topic, StorageLevels.MEMORY_AND_DISK)
 
-    
-    return null;
-  }
-  
-  def directlyDataFromKafka(topic:String) ={
-    
+
+
+  def directlyDataFromKafka(topic: String) = {
+
     //create spark streaming..
-    val ssc = new StreamingContext(sc, Seconds(5))
+    val ssc = new StreamingContext(sc, Seconds(1))
     ssc.checkpoint("/tmp")
 
     // Define the Kafka parameters, broker list must be specified
     //val kafkaParams = Map[String, String]("metadata.broker.list" -> "192.168.1.111:9092",
     //  "group.id" -> "test-consumer-group",
-     // "zookeeper.connect" -> "192.168.1.111:2181")
-    val kafkaParams = Map[String, String]("metadata.broker.list" -> "192.168.1.113:9092")
-        //"group.id" -> "0"
- 
+    // "zookeeper.connect" -> "192.168.1.111:2181")
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> "192.168.1.112:9092")
+    //"group.id" -> "0"
+
     // Define which topics to read from
     val topics = Set(topic)
     //ssc.checkpoint("/tmp")
@@ -51,17 +41,23 @@ class DataReceivers(sc: SparkContext) {
 
     kafkaStream.foreachRDD(rdd => {
 
-      rdd.foreach{
+      //rdd.map(a=>a._1).foreach{ data=> System.out.println(data.toString())}
+      rdd.map(a=> (a, a)).saveToCassandra("kafka_streaming", "airline");
+    }
+    )
+    /*rdd.foreach{
+
 
         data=>  System.out.println("=============="+data.toString()) }
 
-    })
+    }
+    )
 
 
     //val kafkaStream = KafkaUtils.createDirectStream[Array[Byte], Array[Byte], DefaultDecoder, DefaultDecoder](ssc, kafkaParams, topics)
 
     // Define the offset ranges to read in the batch job
-    /**val offsetRanges = Array(
+    /val offsetRanges = Array(
       OffsetRange(topic, 0, 100, 200))
       // Create the RDD based on the offset ranges
     val rdd = KafkaUtils.createRDD[String, String, StringDecoder, StringDecoder](sc, kafkaParams, offsetRanges)
@@ -74,8 +70,19 @@ class DataReceivers(sc: SparkContext) {
       **/
     ssc.start()
     ssc.awaitTermination()
-    
 
+
+  }
+
+
+  def createAirelineSpace() = {
+    /** Creates the keyspace and table in Cassandra. */
+    CassandraConnector(sc.getConf).withSessionDo { session =>
+      session.execute(s"DROP KEYSPACE IF EXISTS kafka_streaming")
+      session.execute(s"CREATE KEYSPACE IF NOT EXISTS kafka_streaming WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1 }")
+      session.execute(s"CREATE TABLE IF NOT EXISTS kafka_streaming.airline (id TEXT PRIMARY KEY, city TEXT)")
+      session.execute(s"TRUNCATE kafka_streaming.airline")
+    }
   }
 }
 
